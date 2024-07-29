0d29023a0691f2e0fa4c2fe048b711684933d037
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-07-29 18:42:37 -0500

    import class "calculadora"

---
+++ app/Calculator.py
@@ -0,0 +1,9 @@
+import click
+
+from app.model import Calculadora
+
+@click.group()
+@lick.pass_context
+def calc(ctx: click.Context)
+    """A simple calculator"""
+    ctx.obj = {"calculator_object": Calculadora()}
