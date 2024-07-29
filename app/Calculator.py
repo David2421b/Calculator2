a9247134016636ac876d76a0978ec23975ae13c9
David Hernandez Mejia <davidmejia0951@gmail.com>
2024-07-29 18:48:17 -0500

    import click

--- app/Calculator.py
+++ app/Calculator.py
@@ -4,6 +4,6 @@ from app.model import Calculadora
 
 @click.group()
 @lick.pass_context
-def calc(ctx: click.Context)
+def calc(ctx: click.Context):
     """A simple calculator"""
     ctx.obj = {"calculator_object": Calculadora()}
